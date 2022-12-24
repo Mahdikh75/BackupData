84f808dce7448ac8336290cc76f49e3f5b841879
Mahdi <Mahdi.khayamdar00@gmail.com>
2022-12-25 02:04:00 +0330

    Libraryes

---
+++ Backup/Librarys/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Backup.Librarys
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void SetPropertyChange([CallerMemberName] string Name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
